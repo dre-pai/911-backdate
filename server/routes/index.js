84c16587c870e098f591e297b5ed36a8dec09f07
Andre Afsharnive <andre.afsharnive@pepperdine.edu>
2020-02-11 16:15:45 -0800

    Updates files for test deploy to EC2

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/message', function(req, res, next) {
-  res.json('Welcome To React');
+  res.json('Welcome To 911 Backdate');
 });
 
 module.exports = router;
