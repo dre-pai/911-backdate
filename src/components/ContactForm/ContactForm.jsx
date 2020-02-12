923b7b0004efcf42d161b9b1f540368730495bf7
Andre Afsharnive <andre.afsharnive@pepperdine.edu>
2020-02-11 22:56:13 -0800

    Removes axios

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -19,7 +19,7 @@ class ContactForm extends React.Component {
       document.getElementById('submit').disabled = false;
     };
 
-    /*let data = {
+    let data = {
       name: name,
       email: email,
       comments: comments
@@ -45,10 +45,11 @@ class ContactForm extends React.Component {
         }
       })
       .catch(function(err) {
+        submitComplete();
         console.log(err);
-      });*/
+      });
 
-    axios
+    /*axios
       .post('/api/send', {
         name,
         email,
@@ -61,7 +62,7 @@ class ContactForm extends React.Component {
       .catch(function(error) {
         submitComplete();
         console.log(error);
-      });
+      });*/
   }
 
   render() {
