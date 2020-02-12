bced7413b7880d7df2f1cb0bdaebfcaa2fe7f294
Andre Afsharnive <andre.afsharnive@pepperdine.edu>
2020-02-11 22:23:55 -0800

    Adds test get route to api

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,7 @@ router.get('/message', function(req, res, next) {
   res.json('Welcome To 911 Backdate');
 });
 
-router.route('/test').post((req, res) => res.json('post worked'));
+router.get('/test', (req, res) => res.json('post worked'));
 
 router.post('/send', (req, res) => {
   let name = req.body.name;
