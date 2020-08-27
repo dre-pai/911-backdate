09ac1f53d3922692dd49f417d963b5ff84f83cff
Andre Afsharnive <andre.afsharnive@pepperdine.edu>
2020-08-26 22:17:40 -0700

    fixes safari footer bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import Routes from './components/Routes/Routes';
 import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-
   render() {
     return (
       <Container>
