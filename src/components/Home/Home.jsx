3cdec94ac1a97da4bffc5c6c789814daa22da1a1
Andre Afsharnive <andre.afsharnive@pepperdine.edu>
2020-11-11 10:03:51 -0800

    attempts to fix autoplay on mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,15 @@ import { Row } from 'react-bootstrap';
 import './Home.css';
 
 class Home extends React.Component {
+  componentDidMount() {
+    document.getElementById('hero').play();
+  }
+
   render() {
     return (
       <div>
         <Row id="hero-container">
-          <video id="hero" src="videos/blue360.mp4" playsinline  autoplay="autoplay" loop="loop" muted="muted"></video>
+          <video id="hero" src="videos/blue360.mp4" playsinline="playsinline" autoplay="autoplay" loop="loop" muted="muted"></video>
         </Row>
         <Row id="orders">
           <a href="/reservations" id="orders-now" className="orders-text">
